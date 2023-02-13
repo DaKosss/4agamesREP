30f194c09c84c61e0dddd1f56ac1fc38861ca8f6
DaKss <112929982+DaKosss@users.noreply.github.com>
2023-02-13 14:10:23 +0600

    Danil's_menu

---
+++ 4aGames/Assets/Scripts/escMenu.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class escMenu : MonoBehaviour
+{
+    public AudioListener audioListener;
+    public GameObject CanvasMenu;
+    public GameObject CanvasUI;
+    public static bool GameIsPaused = false;
+    // Start is called before the first frame update
+    public void ExitMainMenuGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void ContinueGame()
+    {
+        CanvasMenu.SetActive(false);    
+        CanvasUI.SetActive(true);
+        Time.timeScale = 1.0f;
+        GameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void PauseGame()
+    {
+        CanvasMenu.SetActive(true);
+        CanvasUI.SetActive(false);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                ContinueGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+}
