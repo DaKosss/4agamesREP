9195f326dd34c709447ce61389b78d5581d0f900
DaKss <112929982+DaKosss@users.noreply.github.com>
2023-02-24 08:51:33 +0600

    Letter's PickUp fix

--- 4aGames/Assets/Scripts/pickupLetter.cs
+++ 4aGames/Assets/Scripts/pickupLetter.cs
@@ -7,13 +7,15 @@ using UnityEngine.UI;
 public class pickupLetter : MonoBehaviour
 {
     public GameObject collectTextObj, intText;
-    public List<GameObject> collectLists = new List<GameObject>();
+    //public List<GameObject> collectLists = new List<GameObject>();
     //public AudioSource pickupSound, ambianceLayer1, ambianceLayer2, ambianceLayer3, ambianceLayer4, ambianceLayer5, ambianceLayer6, ambianceLayer7, ambianceLayer8;
     public AudioSource collectAudioSource;
     public AudioClip collectAudioClip;
-    public bool interactable;
-    public static int pagesCollected;
+    public bool interactable = false;
+    public static int pagesCollected = 0;
     public Text collectText;
+    public GameObject letterObj;
+
 
     void OnTriggerStay(Collider other)
     {
@@ -33,61 +35,63 @@ public class pickupLetter : MonoBehaviour
     }
     void Update()
     {
+
         if (interactable)
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                pagesCollected = pagesCollected + 1;
+                pagesCollected++;
                 collectText.text = pagesCollected + "/8 pages";
                 collectTextObj.SetActive(true);
                 collectAudioSource.Play();
+                collectAudioSource.PlayOneShot(collectAudioClip);
+                letterObj.SetActive(false);
 
-
-                switch(pagesCollected)
-                {
-                    case 1:
-                        //ambianceLayer1.Play();
-                        collectAudioSource.PlayOneShot(collectAudioClip);
-                        collectLists[0].SetActive(false);
-                        break;
-                    case 2:
-                        //ambianceLayer2.Play();
-                        collectAudioSource.PlayOneShot(collectAudioClip);
-                        collectLists[1].SetActive(false);
-                        break;
-                    case 3:
-                        //ambianceLayer3.Play();
-                        collectAudioSource.PlayOneShot(collectAudioClip);
-                        collectLists[2].SetActive(false);
-                        break;
-                    case 4:
-                        //ambianceLayer4.Play();
-                        collectAudioSource.PlayOneShot(collectAudioClip);
-                        collectLists[3].SetActive(false);
-                        break;
-                    case 5:
-                        //ambianceLayer5.Play();
-                        collectAudioSource.PlayOneShot(collectAudioClip);
-                        collectLists[4].SetActive(false);
-                        break;
-                    case 6:
-                        //ambianceLayer6.Play();
-                        collectAudioSource.PlayOneShot(collectAudioClip);
-                        collectLists[5].SetActive(false);
-                        break;
-                    case 7:
-                        //ambianceLayer7.Play();
-                        collectAudioSource.PlayOneShot(collectAudioClip);
-                        collectLists[6].SetActive(false);
-                        break;
-                    case 8:
-                        //ambianceLayer8.Play();
-                        collectAudioSource.PlayOneShot(collectAudioClip);
-                        collectLists[7].SetActive(false);
-                        break;
-                }
+                //switch(pagesCollected)
+                //{
+                //    case 1:
+                //        //ambianceLayer1.Play();
+                //        collectAudioSource.PlayOneShot(collectAudioClip);
+                //        collectLists[0].SetActive(false);
+                //        break;
+                //    case 2:
+                //        //ambianceLayer2.Play();
+                //        collectAudioSource.PlayOneShot(collectAudioClip);
+                //        collectLists[1].SetActive(false);
+                //        break;
+                //    case 3:
+                //        //ambianceLayer3.Play();
+                //        collectAudioSource.PlayOneShot(collectAudioClip);
+                //        collectLists[2].SetActive(false);
+                //        break;
+                //    case 4:
+                //        //ambianceLayer4.Play();
+                //        collectAudioSource.PlayOneShot(collectAudioClip);
+                //        collectLists[3].SetActive(false);
+                //        break;
+                //    case 5:
+                //        //ambianceLayer5.Play();
+                //        collectAudioSource.PlayOneShot(collectAudioClip);
+                //        collectLists[4].SetActive(false);
+                //        break;
+                //    case 6:
+                //        //ambianceLayer6.Play();
+                //        collectAudioSource.PlayOneShot(collectAudioClip);
+                //        collectLists[5].SetActive(false);
+                //        break;
+                //    case 7:
+                //        //ambianceLayer7.Play();
+                //        collectAudioSource.PlayOneShot(collectAudioClip);
+                //        collectLists[6].SetActive(false);
+                //        break;
+                //    case 8:
+                //        //ambianceLayer8.Play();
+                //        collectAudioSource.PlayOneShot(collectAudioClip);
+                //        collectLists[7].SetActive(false);
+                //        break;
+                //}
                 intText.SetActive(false);
-                this.gameObject.SetActive(false);
+                //this.gameObject.SetActive(false);
                 interactable = false;
                 if (pagesCollected == 8)
                 {
