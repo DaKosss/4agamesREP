38520953cd378dd7614ae446744ef8c13a747e22
DaKss <112929982+DaKosss@users.noreply.github.com>
2023-02-26 17:57:51 +0600

    AudioOptions_work update

---
+++ 4aGames/Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    public string volumeParameter = "MasterVolume";
+    public AudioMixer mixer;
+    public Slider slider;
+
+    private const float _multiplier = 20f;
+
+    private void Awake()
+    {
+        slider.onValueChanged.AddListener(HandleSliderValueChanged);
+    }
+
+    private void HandleSliderValueChanged(float value)
+    {
+        var volumeValue = Mathf.Log10(value) * _multiplier;
+        mixer.SetFloat(volumeParameter, volumeValue);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
